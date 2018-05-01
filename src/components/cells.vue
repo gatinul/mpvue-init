952eddc4909e55053b16077f307b6b0919c69024
gatinul <13252716435@163.com>
2018-05-01 11:18:54 +0800

    mpvue初始化项目

---
+++ src/components/cells.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="ui cells" :class="className">
+      <div class="ui cell" v-for="(item, index) in dataSource" :key="index">
+        <div>{{item.title}}</div>
+        <div class="ui cell_in-access"></div>
+        <dot v-if="item.dot" :text="item.dot"></dot>
+      </div>
+  </div>
+
+</template>
+
+<script>
+import dot from '@/components/dot'
+
+export default {
+  props: ['dataSource', 'className'],
+  components: {
+    dot
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/dot.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div v-if="text" style="padding-bottom:.2rem">
+      <div class="ui badge" style="margin-left: 5px;">{{text}}</div>
+    </div>
+    <div v-else>
+      <div class="" style="font-size: 0">
+        <div style="display: inline-block;vertical-align:middle; font-size: 17px;"></div>
+        <div class="ui badge badge_dot" style="margin-left: 5px;margin-right: 5px;"></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['text']
+}
+</script>
+
+<style lang="css">
+</style>
